0e8cf202617efd117e3ae1cdf0289e1942615c9c
RobertKaribov <infobox.markets@gmail.com>
2024-06-03 19:50:22 +0300

    1

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,12 +1,10 @@
 public class Cat {
     private String name;
 
-    // Геттер по имени
     public String getName() {
         return name;
     }
 
-    // Сеттер для имени
     public void setName(String name) {
         this.name = name;
     }
